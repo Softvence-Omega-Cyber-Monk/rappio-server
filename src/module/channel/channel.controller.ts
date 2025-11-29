5e30ba86ffad21b904a9a598427c27aadd20c485
Tareq Hasan <tareqhasan382@gmail.com>
2025-11-29 09:47:28 +0600

    implement-Onboard a Creator: Create the initial channel profile

--- src/module/channel/channel.controller.ts
+++ src/module/channel/channel.controller.ts
@@ -1,19 +1,53 @@
 import {
+  Body,
   Controller,
   Get,
   HttpCode,
   HttpStatus,
   Param,
+  Post,
+  Req,
   Res,
+  UseGuards,
 } from '@nestjs/common';
 import { ChannelService } from './channel.service';
 import { Response } from 'express';
 import sendResponse from '../../utils/sendResponse';
+import { CreateChannelDto } from './dto/create-channel.dto';
+import { GetMe } from '../user/dto/request-with-user.interface';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 
 @Controller('channels')
 export class ChannelController {
   constructor(private readonly channelService: ChannelService) {}
+
+  @UseGuards(JwtAuthGuard)
+  @Post('onboard')
+  @HttpCode(HttpStatus.CREATED)
+  //@ApiBearerAuth()
+  @ApiOperation({ summary: 'Onboard a Creator: Create the initial channel profile' })
+  //@ApiResponse({ status: 201, description: 'Channel created successfully.', type: CreatorChannelResponseDto })
+  @ApiResponse({ status: 400, description: 'User already owns a channel.' })
+  @ApiResponse({ status: 401, description: 'Unauthorized.' })
+  @ApiResponse({ status: 403, description: 'Forbidden (User is not a CREATOR).' })
+  async onboardCreator(@Req() req: GetMe, @Body() body: CreateChannelDto) {
+    const userId = req.user.id; // User ID attached by the AuthGuard
+    //console.log("userId---------->", userId);
+    // Call the factory method you already created in ChannelService
+    const channel = await this.channelService.createChannelForCreator(
+      userId,
+      body.name,
+    );
+
+    return {
+      message: 'Channel created successfully. You can now access /creator/channel endpoints.',
+      channelHandle: channel.handle,
+      channelId: channel.id,
+    };
+  }
+
 
   /**GET /channels/:handle - Public view of a channel profile and content list.*/
   @Get(':handle')
--- src/module/channel/dto/create-channel.dto.ts
+++ src/module/channel/dto/create-channel.dto.ts
@@ -1,1 +1,10 @@
-export class CreateChannelDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString, MaxLength } from 'class-validator';
+
+export class CreateChannelDto {
+  @ApiProperty({ description: 'The desired public name for the new channel (e.g., "TechGuru Stream")', maxLength: 100, example: 'Awesome Gaming Channel', })
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(100)
+  name: string;
+}
