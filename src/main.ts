149b7b91337b81078a45c1dcf3b7de62aec293d6
Tareq Hasan <tareqhasan382@gmail.com>
2025-12-09 09:39:25 +0600

    aaded cors origin

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
   });
 
   app.enableCors({
-    origin: ['http://localhost:5173','*'],
+    origin: ['http://localhost:5173','https://rappio-tv.netlify.app','*'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
